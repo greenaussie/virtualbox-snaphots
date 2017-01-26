22fc7a164a0df03f2bad1c8f89c7e77e730aa041
Richard Green <richard.green@unsw.edu.au>
2017-01-26 11:10:48 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Virtual box snapshots cron
+
+This is a fairly simply cron which will create snapshots for all running virtualbox guest VMS, for all user accounts on the system with home directories in ``/home``. It is not needed to back up stopped VMs because (a) they don't change and (b), normal file system backups of the images will result in consistant images which can be restarted.
+
+## Usage
+
+Assuming you need this because you are using Virtualbox, simply drop the script into /etc/cron.daily.
---
+++ virtualbox-snapshots.sh
@@ -0,0 +1,25 @@
+#/bin/sh
+
+set -x
+
+# Take snapshots of all running virtualbox vms for all users - a daily cron
+
+# For each user, determined by a list of home directories and assuming a username to home directory name mapping
+for u in $(find /home -mindepth 1 -maxdepth 1 -type d | xargs -i basename {}) ; do
+
+  # Determine which guest vms are running for user
+  for runningvm in $( sudo -u $u vboxmanage list runningvms | sed -r 's/.*(\{.*\})/\1/' ); do 
+
+    # For each running guest vm, delete all but the last two snapshots (takes a while to delete them!)
+    # because we should be backing up the volume of the host, we only need a couple of these because we can
+    # easily restore from the host volume to go back in time
+    for snapid in $( sudo -u $u vboxmanage snapshot $runningvm list | head -n -2 | sed -r 's/.*UUID: (.*)\)/\1/'); do
+      sudo -u $u vboxmanage snapshot $runningvm delete $snapid
+    done
+
+    # foreach running vm create a new snapshot
+    sudo -u $u vboxmanage snapshot $runningvm take "Snapshop - $runningvm $(date)"
+  done
+done
+
+set +x
